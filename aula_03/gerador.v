03de52ba7cc0a26fe1fc13865a57d8f623ae7477
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2020-10-12 11:10:45 -0300

    alterações e exercicios

--- aula_03/gerador.v
+++ aula_03/gerador.v
@@ -1,11 +1,14 @@
 module test;
 
-reg clk = 0, clk2 = 0;
+reg clk;
+reg clk2;
 
-wire clk_wire, clk_wire2;
+wire clk_wire;
+wire clk_wire2;
 
 assign clk_wire = clk;
 assign clk_wire2 = clk2;
+
 
 always #2 begin
     clk <= ~clk;
@@ -15,10 +18,16 @@ always #3 begin
     clk2 <= ~clk2;
 end
 
+
 initial begin
     $dumpvars;
+    clk <= 0;
+    clk2 <= 0;
+
     #500;
+
     $finish;
+    
 end
 
 endmodule
--- exercicios/cadeado_03/cadeado.v
+++ exercicios/cadeado_03/cadeado.v
@@ -6,18 +6,17 @@ module cadeado(
     input d
 );
 
-    reg [15:0] conta_c = 0;
-    reg [15:0] conta_d = 0;
+reg [15:0] conta_c = 0;
+reg [15:0] conta_d = 0;
 
-    always @( posedge c ) begin
-        conta_c <= conta_c + 1;
-    end
+always @( posedge c ) begin
+    conta_c <= conta_c + 1;
+end
 
-    always @( negedge d) begin
-        conta_d <= conta_d + 1;
-    end
+always @( negedge d) begin
+    conta_d <= conta_d + 1;
+end
 
-    assign aberto = a & b & 
-           conta_c == 10 & conta_d == 5;
+assign aberto = a & b & conta_c == 10 & conta_d == 5;
 
 endmodule
