013eb45dd0efd65dfa8ee0a9e8bf147ede3ed32f
Alisson Peloso <alisson.luan2000@gmail.com>
2020-12-02 17:45:26 -0300

    update

--- exercicios/cadeado_05/cadeado.v
+++
@@ -1,9 +0,0 @@
-module cadeado(
-    output aberto,
-    input [3:0] a,
-    input [3:0] b
-);
-
-    assign aberto = (a ^ b) == 4'hA;
-
-endmodule
--- projeto_padilha/tristate.v
+++
@@ -1,9 +0,0 @@
-module tristate(
-    input c,
-    input [5:0] A,
-    output [5:0] B
-);
-
-assign B = c==1? A : 5'bzzzzz;
-
-endmodule
